40e82f9d0d16ae4ffeef9afc0ca33930abeb424c
Alexander Balan <a.balan.job@gmail.com>
2018-09-15 21:04:00 +0300

    Implements Sorter class

--- src/index.js
+++ src/index.js
@@ -1,31 +1,80 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
 
-  add(element) {
-    // your implementation
-  }
+    constructor() {
+        this.array = [];
+        this.compareFunction;
+    }
 
-  at(index) {
-    // your implementation
-  }
+    add(element) {
+        if (typeof element === 'object') {
+            this.convertObjectType(element)
+        } else if (typeof element === 'symbol') {
+            this.addToArray(element);
+        } else if (typeof Number(element) === 'number') {
+            this.addToArray(element);
+        }
+    }
 
-  get length() {
-    // your implementation
-  }
+    convertObjectType(el) {
+        if (el instanceof Number) {
+            el = el.toPrimitive;
+        }
+        this.addToArray(el);
+    }
 
-  toArray() {
-    // your implementation
-  }
+    addToArray(el) {
+        this.array.push(el);
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    at(index) {
+        return this.array[index]
+    }
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+    get length() {
+        return this.array.length;
+    }
+
+    toArray() {
+        return this.array;
+    }
+
+    sort(indices) {
+        if (typeof this.compareFunction ==='function') {
+          return this.compareFunctionSorting(indices);
+        } else {
+          return this.defaultSorting(indices);
+        }
+    }
+
+    defaultSorting(indices) {
+        return this.array.sort((a, b) => {
+            const aIdx = this.array.indexOf(a);
+            const bIdx = this.array.indexOf(b);
+
+            if (indices.includes(aIdx && bIdx)) {
+                return a - b
+            }
+
+            return;
+        });
+    }
+
+    compareFunctionSorting(indices) {
+        return this.array.sort((a, b) => {
+            const aIdx = this.array.indexOf(a);
+            const bIdx = this.array.indexOf(b);
+
+            if (indices.includes(aIdx) && indices.includes(bIdx)) {
+                return this.compareFunction.call(this, a, b);
+            }
+
+            return;
+        })
+    }
+
+    setComparator(compareFunction) {
+        this.compareFunction = compareFunction;
+    }
 }
 
 module.exports = Sorter;
